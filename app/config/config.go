5dbe4a442dd6051576d09a148a4e0aea92be2893
Tai X. <xuantain@gmail.com>
2026-04-04 14:03:28 +0700

    Embed config file to the build in files > so in the built binary file, it can use the config instead of relative path

--- app/config/config.go
+++ app/config/config.go
@@ -1,10 +1,17 @@
 package config
 
 import (
+	"bytes"
+	"embed"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/spf13/viper"
 )
+
+//go:embed config_default.yml config_dev.yml config_prod.yml config_test.yml
+var embeddedConfigs embed.FS
 
 var config *viper.Viper
 
@@ -14,41 +21,54 @@ func Init(env string) {
 	var err error
 	config = viper.New()
 	config.SetConfigType("yaml")
-	config.SetConfigName("config_default")
-	config.AddConfigPath("config/")
-	err = config.ReadInConfig()
+
+	// Load default config from embedded FS
+	defaultData, err := embeddedConfigs.ReadFile("config_default.yml")
 	if err != nil {
-		log.Fatal("error on parsing default configuration file")
+		log.Fatal("error reading embedded default config: ", err)
+	}
+	if err = config.ReadConfig(bytes.NewReader(defaultData)); err != nil {
+		log.Fatal("error parsing default config: ", err)
 	}
 
+	// Merge env-specific config from embedded FS
+	envFile := fmt.Sprintf("config_%s.yml", env)
+	envData, err := embeddedConfigs.ReadFile(envFile)
+	if err != nil {
+		log.Fatal("error reading embedded env config: ", err)
+	}
 	envConfig := viper.New()
 	envConfig.SetConfigType("yaml")
-	envConfig.SetConfigName("config_" + env)
-	envConfig.AddConfigPath("config/")
-	err = envConfig.ReadInConfig()
-	if err != nil {
-		log.Fatal("error on parsing env configuration file")
+	if err = envConfig.ReadConfig(bytes.NewReader(envData)); err != nil {
+		log.Fatal("error parsing env config: ", err)
+	}
+	config.MergeConfigMap(envConfig.AllSettings())
+
+	// Merge parameters.yml from disk (secrets / deployment-specific overrides)
+	// Search order: next to the binary, then ./config/, then config/ relative to cwd
+	paramsPaths := []string{
+		"parameters.yml",
+		"config/parameters.yml",
+	}
+	if exe, err := os.Executable(); err == nil {
+		paramsPaths = append([]string{exe + "/../parameters.yml"}, paramsPaths...)
 	}
 
 	params := viper.New()
 	params.SetConfigType("yaml")
-	params.SetConfigName("parameters")
-	params.AddConfigPath("config/")
-	err = params.ReadInConfig()
-	if err != nil {
-		log.Fatal("error on parsing parameters file")
+	loaded := false
+	for _, p := range paramsPaths {
+		params.SetConfigFile(p)
+		if err := params.ReadInConfig(); err == nil {
+			config.MergeConfigMap(params.AllSettings())
+			loaded = true
+			break
+		}
 	}
-
-	config.MergeConfigMap(envConfig.AllSettings())
-	config.MergeConfigMap(params.AllSettings())
+	if !loaded {
+		log.Println("warning: parameters.yml not found, using defaults only")
+	}
 }
-
-// func relativePath(basedir string, path *string) {
-// 	p := *path
-// 	if len(p) > 0 && p[0] != '/' {
-// 		*path = filepath.Join(basedir, p)
-// 	}
-// }
 
 func GetConfig() *viper.Viper {
 	return config
